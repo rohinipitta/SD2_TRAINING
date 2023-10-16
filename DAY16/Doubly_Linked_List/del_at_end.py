f3788c5f11e02eee008026e44d0fd66b014e0aee
Rohini Pitta <123640441+rohinipitta@users.noreply.github.com>
2023-10-16 22:08:58 +0530

    del_at_end.py
    The provided code represents a doubly linked list in Python, which allows the insertion of elements at the beginning of the list. It includes a `Node` class for node representation and a `DoublyLinkedList` class for list management. The code inserts elements at the beginning and displays the list. It also has a `delete_at_beginning` method for removing elements from the start of the list.

---
+++ DAY16/Doubly_Linked_List/del_at_end.py
@@ -0,0 +1,69 @@
+# Define a Node class for the elements in the doubly linked list
+class Node:
+    def __init__(self, val=0):
+        self.val = val
+        self.next = None
+        self.prev = None
+
+# Define a DoublyLinkedList class
+class DoublyLinkedList:
+    def __init__(self):
+        # Initialize an empty doubly linked list with head and tail nodes
+        self.head = None
+        self.tail = None
+
+    def insert_at_beginning(self, data):
+        # Create a new node with the given data
+        new_node = Node(data)
+        
+        # Handle the case when the list is empty
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            # Update the new node's next pointer to the current head
+            new_node.next = self.head
+            # Update the current head's previous pointer to the new node
+            self.head.prev = new_node
+            # Update the head to the new node
+            self.head = new_node
+
+    def delete_at_beginning(self):
+        if self.head is None:
+            return None
+        
+        # Get the value of the node to be deleted
+        deleted_value = self.head.val
+
+        if self.head == self.tail:
+            # If there's only one node in the list
+            self.head = None
+            self.tail = None
+        else:
+            # Update the head to the next node
+            self.head = self.head.next
+            self.head.prev = None
+
+        return deleted_value
+
+    def display(self):
+        # Display the elements in the doubly linked list
+        temp = self.head
+        while temp:
+            print(temp.val)
+            temp = temp.next
+
+# Example usage with list input
+l = [2, 4, 6, 8, 9]
+dll = DoublyLinkedList()
+
+for i in l:
+    # Insert each element at the beginning of the doubly linked list
+    dll.insert_at_beginning(i)
+
+# Display the doubly linked list
+dll.display()
+
+# Delete an element from the beginning of the list
+deleted_value = dll.delete_at_beginning()
+print("Deleted value at the beginning:", deleted_value)
