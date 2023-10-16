c6f1f805a5c9be2050a2be8c55b145ad482420fe
Rohini Pitta <123640441+rohinipitta@users.noreply.github.com>
2023-10-16 21:52:09 +0530

    insert_at_begin.py
    The provided code is an implementation of a doubly linked list in Python, emphasizing the insertion of elements at the beginning of the list. It includes a `Node` class for representing list elements and a `DoublyLinkedList` class for list management. The `insert_at_beginning` method adds new elements to the beginning of the list, updating the necessary pointers for doubly linked lists.

---
+++ DAY16/insert_at_begin.py
@@ -0,0 +1,46 @@
+# Define a Node class for the elements in the doubly linked list
+class Node:
+    def __init__(self, val=0):
+        self.val = val
+        self.next = None
+        self.prev = None
+
+# Define a DoublyLinkedList class
+class DoublyLinkedList:
+    def __init__(self):
+        # Initialize an empty doubly linked list with head and tail nodes
+        self.head = None
+        self.tail = None
+
+    def insert_at_beginning(self, data):
+        # Create a new node with the given data
+        new_node = Node(data)
+        
+        # Handle the case when the list is empty
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            # Update the new node's next pointer to the current head
+            new_node.next = self.head
+            # Update the current head's previous pointer to the new node
+            self.head.prev = new_node
+            # Update the head to the new node
+            self.head = new_node
+
+    def display(self):
+        # Display the elements in the doubly linked list
+        temp = self.head
+        while temp:
+            print(temp.val)
+            temp = temp.next
+
+# Example usage:
+l = [2, 4, 6, 8, 9]
+dll = DoublyLinkedList()
+
+for i in l:
+    # Insert each element at the beginning of the doubly linked list
+    dll.insert_at_beginning(i)
+
+dll.display()
